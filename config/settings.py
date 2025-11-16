692505342afb9f6d10a75ef466b7c2b2d6f8c046
jijopjames <jijojames14@gmail.com>
2025-11-16 22:42:58 +0530

    Created Config - Settings files.

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+import os
+
+# ------------------ Root Path ------------------
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# ------------------ Data Paths ------------------
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
+PROCESSED_DATA_DIR = os.path.join(RAW_DATA_DIR, "processed")
+
+
+def ensure_directories():
+    os.makedirs(RAW_DATA_DIR, exist_ok=True)
+    os.makedirs(PROCESSED_DATA_DIR, exist_ok=True)
